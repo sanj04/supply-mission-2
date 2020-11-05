42f46401294a71cc214e4f835ae123b8a95af35c
sanj04 <68158087+sanj04@users.noreply.github.com>
2020-11-05 15:12:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,102 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground,box;
+var box1,box2;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG= loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	//box1= new box1(200,200,50,50);
+
+
+	var ground_options ={
+		isStatic: true
+		
+    }
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+	
+	boxSprite=createSprite(width/2, 650, 300,10);
+	boxSprite.shapeColor=color(255)
+
+	box1Sprite=createSprite(550, 560, 10,150);
+	box1Sprite.shapeColor=color(255)
+
+	box2Sprite=createSprite(250, 560, 10,150);
+	box2Sprite.shapeColor=color(255)
+
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	 
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 620, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+	 
+    
+	 box = Bodies.rectangle(width/2, 620, width, 10 , {isStatic:true} );
+	 World.add(world, box);
+
+	 box1 = Bodies.rectangle(width/2, 620, width, 10 , {isStatic:true} );
+	 World.add(world, box1);
+
+	 box2 = Bodies.rectangle(width/2, 620, width, 10 , {isStatic:true} );
+	 World.add(world, box2);
+	Engine.run(engine);
+    keyPressed();   
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  //boxSprite.x= boxBody.position.x 
+  //boxSprite.y= boxBody.position.y
+  drawSprites();
+ 
+
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	//packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:false});
+	//ground = Bodies.rectangle(width/2, 620, width, 10 , {isStatic:true} );
+	//World.add(world, packageBody); 
+
+	 //restitution:0.5
+    // Look at the hints in the document and understand how to make the package body fall only on
+    Matter.Body.setStatic(packageBody,false)
+  }
+}
+
+
+
